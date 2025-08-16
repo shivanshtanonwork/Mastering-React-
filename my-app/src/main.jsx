4a408ab1016cf46e9e5dff48cafa374085accd4d
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-16 12:30:17 +0530

    Writing React Code from Scratch

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDom from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const myFirstElement = <h1>My First Page</h1>
+
+ReactDom.createRoot(document.getElementById("root")).render(myFirstElement)
