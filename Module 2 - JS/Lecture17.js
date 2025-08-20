874c36f2c287bad6c813856dbaf4af9e874fa07f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-20 17:27:18 +0530

    Boolean values in JS

---
+++ Module 2 - JS/Lecture17.js
@@ -0,0 +1,22 @@
+// Boolean values in JS
+// 0 -> False
+// 1 -> True
+
+// these are falsy values it wont run
+// if (0) console.log("Won't Run!");
+// if ("") console.log("Won't Run");
+// if (null) console.log("Won't Run");
+// if (undefined) console.log("Won't Run");
+
+// these are Truthy values
+// if (1) console.log("Runs!");
+// if ("Hello") console.log("Runs!");
+// if (42) console.log("Runs");
+// if ([]) console.log("Runs");
+// if ({}) console.log("Runs");
+
+console.log(Boolean(1));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(undefined));
+console.log(Boolean(null));
