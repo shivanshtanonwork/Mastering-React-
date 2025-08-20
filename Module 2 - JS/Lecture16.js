316642dcf7b2e4e0763e7bccf8033f05477e0fa6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-20 13:34:01 +0530

    Ternary and Short circuiting

---
+++ Module 2 - JS/Lecture16.js
@@ -0,0 +1,9 @@
+const age = 26;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status);
+
+const input = "" || "Default value";
+console.log(input);
+
+const isLoggedIn = true;
+isLoggedIn && console.log("Welcome back");
