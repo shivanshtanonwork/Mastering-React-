b8f6b15bd0e544efb89305b666bbfb38aa9a80f6
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-26 17:34:48 +0530

    Memoization

---
+++ Module 2 - JS/Lecture23.js
@@ -0,0 +1,38 @@
+// Memoization
+
+// function slowAdd(a, b) {
+//     for (let i = 0; i < 1e8; i++) { }
+//     return a + b
+
+// }
+// console.time("No memo")
+// console.log(slowAdd(5, 10));
+// console.timeEnd("No memo")
+
+// console.time("No memo again")
+// console.log(slowAdd(5, 10));
+// console.timeEnd("No memo again")
+
+function memoizedAdd() {
+  const cache = {};
+  return function (a, b) {
+    const key = `${a}_${b}`;
+    if (cache[key]) {
+      return cache[key];
+    }
+    for (let i = 0; i < 1e8; i++) {}
+    const result = a + b;
+    cache[key] = result;
+    return result;
+  };
+}
+
+const add = memoizedAdd();
+
+console.time("With memo");
+console.log(add(5, 10));
+console.timeEnd("With memo");
+
+console.time("with memo again");
+console.log(add(5, 10));
+console.timeEnd("with memo again");
