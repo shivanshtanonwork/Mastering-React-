8bc7d4068a2c8b585b11eadbeb57703aa68ba8c9
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-26 16:49:52 +0530

    Async Await

--- Module 2 - JS/Lecture22.js
+++ Module 2 - JS/Lecture22.js
@@ -1,1 +1,31 @@
 // Async Await in JS
+
+// Async makes a function return a Promise.
+// Await pauses the function execution until the promise is resolved or rejected
+
+//Syntax
+// async function myFunction() {
+//   try {
+//     const result = await someAsyncTask();
+//     console.log(result);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+async function fetchUserData() {
+    console.log("Fetching user data");
+    try {
+        const response = await fetch(
+            "https://jsonplaceholder.typicode.com/todos/4"
+        );
+        const user = await response.json();
+        console.log("User loaded", user.title);
+    } catch (err) {
+        console.log("Failed to fetch", err);
+    } finally {
+        console.log("completed");
+    }
+}
+
+fetchUserData();
