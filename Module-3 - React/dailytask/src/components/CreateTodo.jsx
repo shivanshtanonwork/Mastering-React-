68e55298360459e2244b00652019a7226c769b19
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-28 18:39:47 +0530

    setting attributes dynamically in JSX

--- Module-3 - React/dailytask/src/components/CreateTodo.jsx
+++ Module-3 - React/dailytask/src/components/CreateTodo.jsx
@@ -2,13 +2,19 @@ import React from "react";
 
 const CreateTodo = () => {
   const task = 8;
+  const isDisabled = true;
+  //   const styles = {
+  //     color: "red",
+  //   };
   const counTask = () => {
-    task === 0 ? "No Task Available" : `Total Task ${task}`;
+    return task === 0 ? "No Task Available" : `Total Task ${task}`;
   };
   return (
     <>
-      <h1>Tasks: {counTask()}</h1>
-      <button>Add Task</button>
+      <h1 style={{ color: "blue", fontSize: "45px" }} className="btn">
+        Tasks: {counTask()}
+      </h1>
+      <button disabled={isDisabled}>Add Task</button>
     </>
   );
 };
