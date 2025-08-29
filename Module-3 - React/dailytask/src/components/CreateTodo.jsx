64b8f32eef53732e64f74e766b765cfbccab9284
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-08-29 19:21:59 +0530

    handling user input in forms and events

--- Module-3 - React/dailytask/src/components/CreateTodo.jsx
+++ Module-3 - React/dailytask/src/components/CreateTodo.jsx
@@ -2,20 +2,25 @@ import { useState } from "react";
 
 const CreateTodo = () => {
   const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
   const isDisabled = false;
 
   const handleClick = () => {
     setCount(count + 1);
+  };
+  const handleChange = (event) => {
+    setInput(event.target.value);
   };
   return (
     <>
       <h1 style={{ color: "red", fontSize: "45px" }} className="btn">
         Tasks: {count}
       </h1>
-      <input type="text" />
+      <input type="text" onChange={handleChange} />
       <button value="Add Task Button" onClick={handleClick}>
         Add Task
       </button>
+      <h2>{input}</h2>
     </>
   );
 };
